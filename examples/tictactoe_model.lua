e14a4038fc4e7a568b3f419eb6425e95f9f5ade9
stackdump <myork@stackdump.com>
2022-03-24 08:51:33 -0500

    add license

--- examples/tictactoe_model.lua
+++ examples/tictactoe_model.lua
@@ -1,29 +1,20 @@
 domodel("TicTacToe", function (fn, cell, role)
 
-    local function space(row, col)
-        return cell(row..col, 1, 1)
+    local function row(n)
+        return {
+            [0] = cell(n..0, 1, 1),
+            [1] = cell(n..1, 1, 1),
+            [2] = cell(n..2, 1, 1)
+        }
     end
 
     local board = {
-        [0] = {
-            [0] = space(0,0),
-            [1] = space(0,1),
-            [2] = space(0,2)
-        },
-        [1] = {
-            [0] = space(1,0),
-            [1] = space(1,1),
-            [2] = space(1,2)
-        },
-        [2] = {
-            [0] = space(2,0),
-            [1] = space(2,1),
-            [2] = space(2,2)
-        }
+        [0] = row(0),
+        [1] = row(1),
+        [2] = row(2)
     }
 
-    local X = "X"
-    local O = "O"
+    local X, O = "X", "O"
 
     local players = {
         [X] = {
@@ -38,10 +29,10 @@ domodel("TicTacToe", function (fn, cell, role)
         }
     }
 
-    for i, row in pairs(board) do
-        for j in pairs(row) do
-            for label, player in pairs(players) do
-                local move = fn(label..i..j, player.role)
+    for i, board_row in pairs(board) do
+        for j in pairs(board_row) do
+            for marking, player in pairs(players) do
+                local move = fn(marking..i..j, player.role)
                 player.turn.tx(1, move)
                 board[i][j].tx(1, move)
                 move.tx(1, players[player.next].turn)
--- src/metamodel.lua
+++ src/metamodel.lua
@@ -1,8 +1,31 @@
+-- MIT License
+--
+-- Copyright (c) 2022 stackdump.com LLC
+--
+-- Permission is hereby granted, free of charge, to any person obtaining a copy
+-- of this software and associated documentation files (the "Software"), to deal
+-- in the Software without restriction, including without limitation the rights
+-- to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+-- copies of the Software, and to permit persons to whom the Software is
+-- furnished to do so, subject to the following conditions:
+--
+-- The above copyright notice and this permission notice shall be included in all
+-- copies or substantial portions of the Software.
+--
+-- THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+-- IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+-- FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+-- AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+-- LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+-- OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+-- SOFTWARE.
+
 -- model definitions indexed by schema name
 model = {}
 
 -- load a model using internal Lua DSL
 function domodel(schema, declaration)
+
 	local def = {
 		schema = schema,
 		roles = {},
@@ -15,7 +38,7 @@ function domodel(schema, declaration)
 		local transition = {
 			label=label,
 			role=role,
-			position = position, -- extensible table space
+			position = position,
 			guards={},
 			delta={},
 		}
@@ -100,7 +123,7 @@ function domodel(schema, declaration)
 
 	local function capacity_vector()
 		local v = {}
-		for i, p in pairs( def.places ) do
+		for _, p in pairs( def.places ) do
 			v[p.offset] = p.capacity
 		end
 		return v
@@ -131,7 +154,7 @@ function domodel(schema, declaration)
 		local cap = capacity_vector()
 		local out = {}
 		local ok = true
-		for i, s in pairs(state) do
+		for i in pairs(state) do
 			out[i] = state[i] + delta[i] * multiple
 
 			if (out[i] < 0) then
